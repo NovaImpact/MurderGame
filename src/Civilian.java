1a299abbc1635163d0a2a463132ccf920a417277
adilk <adilkabir738@gmail.com>
2025-09-12 09:40:48 -0400

    Civilian push

--- src/Civilian.java
+++ src/Civilian.java
@@ -2,17 +2,18 @@ public class Civilian {
 
     String name;
     boolean dead;
-    String userInteraction;
+    String userDialogue;
 
     void guilty() {
-        System.out.println("Hi, " + name + "is not guilty!");
+        System.out.println( name + " is not guilty!");
     }
 
     void status() {
-        System.out.println("Civilian: " + name + "Civilian Died: " + dead);
+        System.out.println(" Civilian: " + name + " Civilian Dead: " + dead);
     }
 
     void interactionCivilian() {
-        System.out.println(userInteraction);
+        System.out.println(userDialogue);
     }
+
 }
