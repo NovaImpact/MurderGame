42ec91f77b40b467e54c12efb661a87bbcf58e5d
adilk <adilkabir738@gmail.com>
2025-09-12 09:41:15 -0400

    Civilian push

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,54 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome!");
-        System.out.println("You are the murderer.");
-
+        System.out.println("Welcome!You are the murderer.");
 
         Civilian adil = new Civilian();
         adil.name = "Adil";
         adil.dead = false;
-        adil.userInteraction = "Hello!";
+        adil.userDialogue = "Hello!";
+        adil.status();
+        adil.guilty();
+        adil.interactionCivilian();
 
         Civilian sam = new Civilian();
         sam.name = "Sam";
         sam.dead = false;
-        sam.userInteraction = "Hi, how are you?";
+        sam.userDialogue = "Hi, how are you?";
+        sam.status();
+        sam.guilty();
+        sam.interactionCivilian();
 
         Civilian tai = new Civilian();
         tai.name = "Tai";
         tai.dead = false;
-        tai.userInteraction = "What are you doing?";
-
+        tai.userDialogue = "What are you doing?";
+        tai.status();
+        tai.guilty();
+        tai.interactionCivilian();
 
         Murderer minghao = new Murderer();
         minghao.murdererName = "Minghao;";
         minghao.killCounter = 0;
+
         Scanner myTextScanner = new Scanner(System.in);
-        String typedName = myTextScanner.nextLine();
+
         boolean killGUI = true;
         while (killGUI){
+
             System.out.print("Type person to kill");
-            String typeName = myTextScanner.nextLine();
+            String typedName1 = myTextScanner.nextLine();
+            System.out.print(typedName1 + " Died");
+            String typedName2 = myTextScanner.nextLine();
 
 
-
-
-            if (adil.name.equals(typedName)) {
+            if (adil.name.equals(typedName1)) {
                 adil.dead = true;
+                adil.status();
+                minghao.killCounter = 1;
                 System.out.println("Adil died...");
                 }
+
 
 
             }
@@ -48,8 +59,5 @@ public class Main {
 
 
     }
-
-
-
 
 
